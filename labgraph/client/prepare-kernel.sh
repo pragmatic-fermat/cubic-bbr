87952a6ba04725fe1a9413c697ea4bc79867991d
Pragmatic Fermat <141491326+pragmatic-fermat@users.noreply.github.com>
2024-05-08 18:49:48 +0200

    Create prepare-kernel.sh

---
+++ labgraph/client/prepare-kernel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sysctl -w net.ipv4.tcp_no_metrics_save=1
+
+echo "before:"
+ethtool -k eth0 | grep tcp-segmentation-offload
+ethtool -K eth0 tso off
+echo "after:"
+ethtool -k eth0 | grep tcp-segmentation-offload
